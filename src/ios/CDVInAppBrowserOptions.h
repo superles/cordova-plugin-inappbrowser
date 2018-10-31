fd89e96518818d59094df1de85bb36de9e977785
Florian Manicuta <florianmanicuta@gmail.com>
2018-10-31 15:13:12 +0200

    Add support for status bar theme

--- src/ios/CDVInAppBrowserOptions.h
+++ src/ios/CDVInAppBrowserOptions.h
@@ -37,6 +37,7 @@
 
 @property (nonatomic, copy) NSString* presentationstyle;
 @property (nonatomic, copy) NSString* transitionstyle;
+@property (nonatomic, copy) NSString* statusbartheme;
 
 @property (nonatomic, assign) BOOL enableviewportscale;
 @property (nonatomic, assign) BOOL mediaplaybackrequiresuseraction;
